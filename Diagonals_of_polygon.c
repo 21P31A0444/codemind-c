4d48ec5f84440f7e4cf2c7e058a481dc898d8121
21P31A0444 <99868533+21P31A0444@users.noreply.github.com>
2022-02-21 11:22:08 +0530

    /*to print the number of diagonals of the polygon with n sides*/ #include<stdio.h> int main() {     int n;     scanf("%d",&n);     printf("%d",(n*(n-3)/2));     return 0; }

---
+++ Diagonals_of_polygon.c
@@ -0,0 +1,10 @@
+
+/*to print the number of diagonals of the polygon with n sides*/
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    printf("%d",(n*(n-3)/2));
+    return 0;
+}
